6387fd1005a4066df8af872d276d5bf43d2e3343
gwenknight <gwen.knight@lshtm.ac.uk>
2024-07-17 17:40:55 +0100

    Added in table 1 and summary stats for paper

--- 1_initial_data_look_total.R
+++ 1_initial_data_look_total.R
@@ -15,11 +15,11 @@ drugs_lookup <- fread("data/drugs_lookup.csv")
 all_data[drugs_lookup, on="BNF_CHEMICAL_SUBSTANCE_CODE", drug_name := CHEMICAL_SUBSTANCE_BNF_DESCR]
 # set levels on age band
 all_data[, AGE_BAND := factor(AGE_BAND, levels = c("0-1", "2-5", 
-                                                       "6-10", "11-15", "16-20", "21-25", 
-                                                       "26-30", "31-35", "36-40", "41-45", 
-                                                       "46-50", "51-55",  "56-60", "61-65", 
-                                                       "66-70", "71-75", "76-80",  "81-85" ,
-                                                       "86-90", "91-95","96-100","101-105", "105+", "Unknown") )]
+                                                   "6-10", "11-15", "16-20", "21-25", 
+                                                   "26-30", "31-35", "36-40", "41-45", 
+                                                   "46-50", "51-55",  "56-60", "61-65", 
+                                                   "66-70", "71-75", "76-80",  "81-85" ,
+                                                   "86-90", "91-95","96-100","101-105", "105+", "Unknown") )]
 # Split year and month for later use
 all_data[, "YEAR" := as.numeric(substr(YEAR_MONTH, 1, 4)),]
 all_data[, "MONTH" := as.numeric(substr(YEAR_MONTH, 5, 6)),]
@@ -30,56 +30,56 @@ all_data[, UNIQUE_PATIENT_COUNT := as.numeric(UNIQUE_PATIENT_COUNT)]
 all_data[, ITEMS := as.numeric(ITEMS)]
 
 if(sensitivity_choice == "default_1"){
-
+  
   all_data[is.na(UNIQUE_PATIENT_COUNT), UNIQUE_PATIENT_COUNT := 1]
-
+  
   all_data[is.na(ITEMS), ITEMS := 1]
   
 } else if (sensitivity_choice == "sens_4"){
-
+  
   all_data[is.na(UNIQUE_PATIENT_COUNT), UNIQUE_PATIENT_COUNT := 4]
-
+  
   all_data[is.na(ITEMS), ITEMS := 4]
   
 } else if (sensitivity_choice == "m4_f1"){
   
   all_data[is.na(UNIQUE_PATIENT_COUNT) & 
-                 GENDER == "Male", UNIQUE_PATIENT_COUNT := 4]
+             GENDER == "Male", UNIQUE_PATIENT_COUNT := 4]
   all_data[is.na(UNIQUE_PATIENT_COUNT) & 
-                 GENDER == "Female", UNIQUE_PATIENT_COUNT := 1]
+             GENDER == "Female", UNIQUE_PATIENT_COUNT := 1]
   all_data[is.na(UNIQUE_PATIENT_COUNT) & 
-                 GENDER == "Indeterminate", UNIQUE_PATIENT_COUNT := 0]
+             GENDER == "Indeterminate", UNIQUE_PATIENT_COUNT := 0]
   all_data[is.na(UNIQUE_PATIENT_COUNT) & 
-                 GENDER == "Unknown", UNIQUE_PATIENT_COUNT := 0]
+             GENDER == "Unknown", UNIQUE_PATIENT_COUNT := 0]
   
   all_data[is.na(ITEMS) & 
-                 GENDER == "Male", ITEMS := 4]
+             GENDER == "Male", ITEMS := 4]
   all_data[is.na(ITEMS) & 
-                 GENDER == "Female", ITEMS := 1]
+             GENDER == "Female", ITEMS := 1]
   all_data[is.na(ITEMS) & 
-                 GENDER == "Indeterminate", ITEMS := 0]
+             GENDER == "Indeterminate", ITEMS := 0]
   all_data[is.na(ITEMS) & 
-                 GENDER == "Unknown", ITEMS := 0]
+             GENDER == "Unknown", ITEMS := 0]
   
 } else if (sensitivity_choice == "20_cutoff"){
   
   all_data[is.na(UNIQUE_PATIENT_COUNT), UNIQUE_PATIENT_COUNT := 1]
   
   all_data[is.na(UNIQUE_PATIENT_COUNT) & 
-                 AGE_BAND %in% c("0-1", "2-5", "6-10", 
-                                 "11-15", "16-20"), UNIQUE_PATIENT_COUNT := 4]
+             AGE_BAND %in% c("0-1", "2-5", "6-10", 
+                             "11-15", "16-20"), UNIQUE_PATIENT_COUNT := 4]
   
   all_data[is.na(UNIQUE_PATIENT_COUNT) & 
-                 AGE_BAND %in% c("Unknown"), UNIQUE_PATIENT_COUNT := 0]
+             AGE_BAND %in% c("Unknown"), UNIQUE_PATIENT_COUNT := 0]
   
   all_data[is.na(ITEMS), ITEMS := 1]
   
   all_data[is.na(ITEMS) & 
-                 AGE_BAND %in% c("0-1", "2-5", "6-10", 
-                                 "11-15", "16-20"), ITEMS := 4]
+             AGE_BAND %in% c("0-1", "2-5", "6-10", 
+                             "11-15", "16-20"), ITEMS := 4]
   
   all_data[is.na(ITEMS) & 
-                 AGE_BAND %in% c("Unknown"), ITEMS := 0]
+             AGE_BAND %in% c("Unknown"), ITEMS := 0]
   
 } else {
   print("Not a valid sensitivity choice")
@@ -143,7 +143,7 @@ temp_gender_drug[, type := "Gender"]
 
 # combine for plotting
 temp_both_drug <- rbind(temp_gender_drug[GENDER == "Unknown", c("drug_name", "percent", "type", "V1")], 
-                   temp_age_drug[AGE_BAND == "Unknown", c("drug_name", "percent", "type", "V1")])
+                        temp_age_drug[AGE_BAND == "Unknown", c("drug_name", "percent", "type", "V1")])
 
 temp_both_drug_m <- melt.data.table(temp_both_drug, id.vars = c("drug_name", "type"))
 temp_both_drug_m[variable == "V1", variable := "number"]
@@ -170,7 +170,7 @@ UNKNOWN_DRUG <- ggplot(temp_both_drug_m, aes(x = drug_name, y = value, fill = ty
   theme_bw() + 
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
   labs(x = "Antibiotic name")
-  
+
 #save for supplement
 ggsave(paste0("plots/",sensitivity_choice,"/unknown_drug.pdf"), plot = UNKNOWN_DRUG, 
        width = 15, height = 10)
@@ -203,30 +203,30 @@ for(i in unique(all_data_ex$BNF_CHEMICAL_SUBSTANCE_CODE)){
   target_data <- all_data_ex[BNF_CHEMICAL_SUBSTANCE_CODE == target]
   
   # Explore monthly and age variation by year 
- TEMP <- ggplot(target_data,aes(x= AGE_BAND, y = ITEMS, colour = YEAR) ) +
+  TEMP <- ggplot(target_data,aes(x= AGE_BAND, y = ITEMS, colour = YEAR) ) +
     geom_jitter( alpha =0.5) +
     labs(title = target_name,x = "Age Band", y = "Prescription items", 
          colour = "Year") + facet_grid(.~MONTH) + theme_bw() + 
     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
   
-   ggsave(paste0("plots/", sensitivity_choice, "/seasonality/",str_replace_all(target_name, "[^[:alnum:]]", " "),
+  ggsave(paste0("plots/", sensitivity_choice, "/seasonality/",str_replace_all(target_name, "[^[:alnum:]]", " "),
                 "_seasonality_",sensitivity_choice,".pdf"), plot = TEMP, 
          width = 20, height = 10)
- 
+  
   if(sensitivity_choice == "default_1"){
-  # # percentage of data rows that are in the 1-4 category
-  prop_1_4 <- table(target_data$ITEMS == 1)["TRUE"] / dim(target_data)[1]
-  print(paste0(target_name, "--- Proportion of data points with 1-4 items prescribed: ", round(prop_1_4,2)))
-  
-  drugs_lookup[BNF_CHEMICAL_SUBSTANCE_CODE == target, p_1_4 := prop_1_4]
-  
-  # total number of rows in the original dataset
-  drugs_lookup[BNF_CHEMICAL_SUBSTANCE_CODE == target, total_entries := dim(target_data)[1]]
-
+    # # percentage of data rows that are in the 1-4 category
+    prop_1_4 <- table(target_data$ITEMS == 1)["TRUE"] / dim(target_data)[1]
+    print(paste0(target_name, "--- Proportion of data points with 1-4 items prescribed: ", round(prop_1_4,2)))
+    
+    drugs_lookup[BNF_CHEMICAL_SUBSTANCE_CODE == target, p_1_4 := prop_1_4]
+    
+    # total number of rows in the original dataset
+    drugs_lookup[BNF_CHEMICAL_SUBSTANCE_CODE == target, total_entries := dim(target_data)[1]]
+    
   }
-   
-   # total number of prescriptions
-   drugs_lookup[BNF_CHEMICAL_SUBSTANCE_CODE == target, total_prescriptions := sum(target_data$ITEMS)]
+  
+  # total number of prescriptions
+  drugs_lookup[BNF_CHEMICAL_SUBSTANCE_CODE == target, total_prescriptions := sum(target_data$ITEMS)]
 }
 
 ############ Need to normalise prescriptions by the number of people in age group ########################
@@ -235,7 +235,7 @@ for(i in unique(all_data_ex$BNF_CHEMICAL_SUBSTANCE_CODE)){
 ## Up to 2021
 pop_sizes_to_21 <- fread("data/MYEB2_detailed_components_of_change_series_EW_(2020_geog20).csv")
 pop_sizes_to_21 <- pop_sizes_to_21[, c("laname20", "age", "sex", "population_2015", "population_2016", 
-                        "population_2017", "population_2018", "population_2019", "population_2020")]
+                                       "population_2017", "population_2018", "population_2019", "population_2020")]
 
 pop_sizes_to_21[,pop_2015 :=  sum(population_2015), by = c("age", "sex")]
 pop_sizes_to_21[,pop_2016 :=  sum(population_2016), by = c("age", "sex")]
@@ -307,7 +307,7 @@ all_data_ex[AGE_BAND == "105+", AGE_BAND := "86+" ]
 
 # Sum to get number in each by the age groupings and sex
 all_data_ex <- all_data_ex[, sum(ITEMS),
-                                      by = c("BNF_CHEMICAL_SUBSTANCE_CODE", "AGE_BAND","GENDER" ,"YEAR", "MONTH", "drug_name")]
+                           by = c("BNF_CHEMICAL_SUBSTANCE_CODE", "AGE_BAND","GENDER" ,"YEAR", "MONTH", "drug_name")]
 colnames(all_data_ex)[which(colnames(all_data_ex) == "V1")] <- "ITEMS"
 
 # Melt population data to merge with prescription data 
@@ -325,6 +325,10 @@ pop_sizes_all[sex == "M", GENDER := "Male"]
 pop_sizes_all[sex == "F", GENDER := "Female"]
 # Merge population and prescription data 
 all_data_ex[pop_sizes_all, on = c("AGE_BAND", "GENDER", "YEAR"), population := i.value]
+
+## Checks on population 
+# all_data_ex %>% filter(YEAR == 2023, MONTH == 1, drug_name == "Nitrofurantoin") %>%#, #AGE_BAND %in% c("0-1","2-5")) %>%
+#   summarise(sum(population)) ## 60 million 
 
 # Calculate the per 100,000 population prescription rate 
 all_data_ex[, per_100k := (ITEMS/ population)*100000]
@@ -357,32 +361,32 @@ for(i in drugs_lookup$BNF_CHEMICAL_SUBSTANCE_CODE){
   # target <- "0501013B0" # Amox # key examples
   # target <- "0501120P0" # Ofloxacin # key examples
   target_name <- drugs_lookup[BNF_CHEMICAL_SUBSTANCE_CODE == target]$CHEMICAL_SUBSTANCE_BNF_DESCR[1]
-
+  
   target_data <- all_data_ex[BNF_CHEMICAL_SUBSTANCE_CODE == target]
   
   if(nrow(target_data) > 0 ){
-  PLOT_TEMP <-  ggplot(target_data,aes(x= date_time2, y = per_100k, colour = AGE_BAND, group = AGE_BAND) ) +
-    annotate("rect", xmin = as.Date("2020-03-01"), xmax = as.Date("2022-07-01"), ymin = 0, ymax = max(target_data$per_100k),
-             alpha = 0.4,fill = "grey") +
-    geom_point( size =0.3) +
-    geom_line()+
-    labs(title = target_name) + facet_grid(GENDER~.) + 
-    theme_bw() + 
-    labs(x = "Month", y = "Prescriptions per 100k population", colour = "Age band") + 
-    guides(color = guide_legend(override.aes = list(size=2))) +
-  #  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
-    scale_color_manual(values = cc)
-  
-  # Save these for Figure 1 plot below
-  if(target == "0501050B0"){CLARITH_PLOT <- PLOT_TEMP + labs(title= "A: Clarithromycin") }
-  if(target == "0501120P0"){OF_PLOT <- PLOT_TEMP+ labs(title= "B: Ofloxacin")  }
-  
-  print(target_name)
-  
-  ggsave(paste0("plots/",sensitivity_choice,"/per_pop/",str_replace_all(target_name, "[^[:alnum:]]", " "),
-                "_overview_",sensitivity_choice,".pdf"), plot = PLOT_TEMP, 
-         width = 20, height = 10)
-
+    PLOT_TEMP <-  ggplot(target_data,aes(x= date_time2, y = per_100k, colour = AGE_BAND, group = AGE_BAND) ) +
+      annotate("rect", xmin = as.Date("2020-03-01"), xmax = as.Date("2022-07-01"), ymin = 0, ymax = max(target_data$per_100k),
+               alpha = 0.4,fill = "grey") +
+      geom_point( size =0.3) +
+      geom_line()+
+      labs(title = target_name) + facet_grid(GENDER~.) + 
+      theme_bw() + 
+      labs(x = "Month", y = "Prescriptions per 100k population", colour = "Age band") + 
+      guides(color = guide_legend(override.aes = list(size=2))) +
+      #  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) + 
+      scale_color_manual(values = cc)
+    
+    # Save these for Figure 1 plot below
+    if(target == "0501050B0"){CLARITH_PLOT <- PLOT_TEMP + labs(title= "A: Clarithromycin") }
+    if(target == "0501120P0"){OF_PLOT <- PLOT_TEMP+ labs(title= "B: Ofloxacin")  }
+    
+    print(target_name)
+    
+    ggsave(paste0("plots/",sensitivity_choice,"/per_pop/",str_replace_all(target_name, "[^[:alnum:]]", " "),
+                  "_overview_",sensitivity_choice,".pdf"), plot = PLOT_TEMP, 
+           width = 20, height = 10)
+    
   }
 }
 
@@ -428,7 +432,7 @@ relative_weightings <- relative_weightings[!is.na(relative_gender),]
 RELATIVE_GENDER <- ggplot(relative_weightings[YEAR == target_year], aes(x = AGE_BAND, y = drug_name, fill = relative_gender)) + 
   geom_tile() + 
   theme_linedraw() +
-   scale_fill_gradient2(low = "#24693D", mid = "#F4F8FB",high = "#2A5783" ) + 
+  scale_fill_gradient2(low = "#24693D", mid = "#F4F8FB",high = "#2A5783" ) + 
   facet_grid(short_title~., scales = "free_y", space = "free") + 
   labs(x = "Age Band", y= "Antibiotic", fill = "relative rate (log)", 
        title = paste0("C: Relative prescription rate by sex (",target_year,")")) + 
@@ -440,7 +444,7 @@ ggsave(paste0("plots/",sensitivity_choice,"/per_pop/relative_gender_",target_yea
 LEG <- get_legend(CLARITH_PLOT) 
 
 FIG1 <- grid.arrange(CLARITH_PLOT + theme(legend.position = "NONE"), OF_PLOT+ theme(legend.position = "NONE"), LEG ,RELATIVE_GENDER, layout_matrix = rbind(c(1,1,1,1,1, 3, 4,4,4,4,4,4,4),
-                                                                             c(2,2,2,2,2, 3, 4,4,4,4,4,4,4))
+                                                                                                                                                           c(2,2,2,2,2, 3, 4,4,4,4,4,4,4))
 )
 
 
@@ -448,3 +452,139 @@ FIG1 <- grid.arrange(CLARITH_PLOT + theme(legend.position = "NONE"), OF_PLOT+ th
 ggsave(paste0("plots/",sensitivity_choice,"/Fig1_",sensitivity_choice,".pdf"), plot = FIG1, 
        width = 20, height = 10)
 
+################## Table 1 #########################
+all_data_ex <- fread(paste0("data/",sensitivity_choice,"/all_data_organised_",sensitivity_choice,".csv"))
+
+# Summary values for results section 
+# Total 
+format(all_data_ex %>% summarise(sum(ITEMS)), big.mark = ",")
+# Per person 
+total_pops = all_data_ex %>% group_by(YEAR) %>% filter(MONTH == 12, drug_name == "Nitrofurantoin") %>% 
+  summarise(totalp = sum(population))
+
+per_persond <- all_data_ex %>% group_by(YEAR) %>% summarise(total_items = sum(ITEMS)) %>%
+  left_join(total_pops) %>% mutate(per_person = total_items / totalp)
+
+paste0(round(mean(per_persond$per_person),2), "(",
+       round(min(per_persond$per_person),2),", ",
+       round(max(per_persond$per_person),2), ")")
+
+
+# In women 
+inwomend <- all_data_ex %>% group_by(GENDER, YEAR) %>% summarise(total_items = sum(ITEMS)) %>%
+  pivot_wider(names_from = GENDER, values_from = total_items) %>%
+  mutate(perc_f = 100*Female / (Male + Female))
+
+paste0(round(mean(inwomend$perc_f),0), "(",
+       round(min(inwomend$perc_f),0),", ",
+       round(max(inwomend$perc_f),0), ")")
+
+# By Gender: amount per person per year 
+total_popsg = all_data_ex %>% group_by(GENDER, YEAR) %>% filter(MONTH == 12, drug_name == "Nitrofurantoin") %>% 
+  summarise(totalp = sum(population))
+
+per_persondg <- all_data_ex %>% group_by(GENDER, YEAR) %>% summarise(total_items = sum(ITEMS)) %>%
+  left_join(total_popsg) %>% mutate(per_person = total_items / totalp) %>%
+  select(GENDER, per_person, YEAR) %>%
+  pivot_wider(names_from = GENDER, values_from = per_person)
+
+paste0(round(mean(per_persondg$Female),2), "(",
+       round(min(per_persondg$Female),2),", ",
+       round(max(per_persondg$Female),2), ")")
+
+paste0(round(mean(per_persondg$Male),2), "(",
+       round(min(per_persondg$Male),2),", ",
+       round(max(per_persondg$Male),2), ")")
+
+# By Age 
+
+aged <- all_data_ex %>% group_by(AGE_BAND, YEAR) %>% 
+  summarise(totalp = sum(ITEMS)) %>% pivot_wider(names_from = AGE_BAND,
+                                                 values_from = totalp) %>%
+  rowwise() %>% 
+  mutate(under_15 = sum(`0-1`,`2-5`,`6-10`,`11-15`),
+         over_65 = sum(`66-70`, `71-75`, `76-80`, `81-85`, `86+`),
+         total = sum(`0-1`, `11-15`, `16-20`, `2-5`, `21-25`, `26-30`, `31-35`, 
+                     `36-40`, `41-45`, `46-50`, `51-55`, `56-60`, `6-10`, `61-65`, 
+                     `66-70`, `71-75`, `76-80`, `81-85`, `86+`),
+         perc_under_15 = 100*under_15/total,
+         perc_over_65 = 100*over_65/total) %>%
+  select(under_15, over_65, total, perc_under_15, perc_over_65)
+
+paste0(round(mean(aged$perc_under_15),0), "%(",
+       round(min(aged$perc_under_15),0),"%, ",
+       round(max(aged$perc_under_15),0), "%)")
+
+paste0(round(mean(aged$perc_over_65),0), "%(",
+       round(min(aged$perc_over_65),0),"%, ",
+       round(max(aged$perc_over_65),0), "%)")
+
+
+
+# Table 1 summary values
+total_p <- all_data_ex %>% group_by(drug_name) %>% summarise(total_prescriptions = sum(ITEMS)) 
+aver_p <- all_data_ex %>% group_by(drug_name, YEAR) %>% summarise(total_per_year = sum(ITEMS)) %>%
+  group_by(drug_name) %>% summarise(mean_annual_prescriptions = round(mean(total_per_year),0))
+top_month <- all_data_ex %>% group_by(drug_name, MONTH) %>% summarise(total_per_month = sum(ITEMS)) %>%
+  group_by(drug_name) %>% mutate(max_monthly = max(total_per_month),
+                                 max_month = ifelse(total_per_month == max_monthly,MONTH,"")) %>%
+  filter(!max_month == "") %>%
+  select(drug_name,max_month) %>%
+  group_by(drug_name) %>%
+  summarise(top_month = paste(max_month, collapse = ",")) 
+
+perc_female <- all_data_ex %>% group_by(drug_name, GENDER) %>% 
+  summarise(total_prescriptions = sum(ITEMS)) %>%
+  pivot_wider(names_from = GENDER, values_from = total_prescriptions) %>%
+  mutate(perc_female = round(100 * Female/(Female + Male),0)) %>% select(perc_female)
+
+top_age_female_2023 <- all_data_ex %>% filter(YEAR == 2023, GENDER == "Female") %>% 
+  group_by(drug_name, AGE_BAND) %>% 
+  summarise(total_pop = sum(population),# have to sum over years
+            total_ITEMS = sum(ITEMS), 
+            per_100k = 100000 * total_ITEMS / total_pop) %>% 
+  arrange(desc(per_100k)) %>% slice(1:3) %>% #filter(drug_name == "Amikacin")
+  group_by(drug_name) %>%
+  arrange(AGE_BAND, .by_group = TRUE) %>% 
+  summarise(top_agesf = paste(AGE_BAND, collapse = ",")) 
+
+top_age_male_2023 <- all_data_ex %>% filter(YEAR == 2023, GENDER == "Male") %>% 
+  group_by(drug_name, AGE_BAND) %>% 
+  summarise(total_pop = sum(population),# have to sum over years
+            total_ITEMS = sum(ITEMS), 
+            per_100k = 100000 * total_ITEMS / total_pop) %>% 
+  arrange(desc(per_100k)) %>% slice(1:3) %>%
+  group_by(drug_name) %>%
+  arrange(AGE_BAND, .by_group = TRUE) %>% 
+  summarise(top_agesm = paste(AGE_BAND, collapse = ",")) 
+
+top_three_years <- all_data_ex %>% filter(YEAR %in% c(2016, 2019, 2023)) %>% 
+  group_by(YEAR, drug_name) %>% summarise(total_prescriptions = sum(ITEMS)) %>%
+  pivot_wider(names_from = YEAR, values_from = total_prescriptions)
+
+table1_unformatted <- total_p %>% left_join(aver_p) %>% left_join(perc_female)  %>% left_join(top_month) %>% left_join(top_three_years) %>%
+  left_join(top_age_female_2023) %>% left_join(top_age_male_2023) %>%
+  ungroup() %>% 
+  arrange(desc(total_prescriptions))
+
+## Format for output
+table1 <- table1_unformatted
+table1$total_prescriptions <- format(table1$total_prescriptions, big.mark = ",")
+table1$mean_annual_prescriptions <- format(table1$mean_annual_prescriptions, big.mark = ",")
+table1$`2016` <- format(table1$`2016`, big.mark = ",")
+table1$`2019` <- format(table1$`2019`, big.mark = ",")
+table1$`2023` <- format(table1$`2023`, big.mark = ",")
+
+colnames(table1) <- c("Antibiotic", "Total prescriptions", "Mean annual prescriptions",
+                      "% to females", 
+                      "Month","2016", "2019","2023",
+                      "Females","Males")
+
+write.csv(table1, paste0("tables/",sensitivity_choice,"_table1.csv"))
+
+### common month
+table(table1[1:20,"Month"])
+table(table1[1:20,"% to females"])
+length(which(table1_unformatted[1:20,"2023"] - table1_unformatted[1:20,"2016"]<0))
+          
+      
